b2efe371889f837eb52ce5b4d3a43178b27a7dad
Manas Kanithahalli Sadananda <manas.ks15@gmail.com>
2015-04-01 19:01:55 -0700

    bit_parity
    Added bit parity function

--- Lab0/bit_parity.v
+++ Lab0/bit_parity.v
@@ -2,7 +2,7 @@ module bit_parity(bit_a, bit_b, bit_parity_out)
 
 input bit_a;
 input bit_b;
-input bit_borrow_in;
 
-output bit_diff;
-output bit_borrow_out;
+output bit_parity_out;
+
+assign bit_parity_out <= bit_a xor bit_b;
