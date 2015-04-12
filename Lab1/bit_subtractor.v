60c90a273c3512c6c446b1df3a74dd51e8bba0f5
Manas Kanithahalli Sadananda <manas.ks15@gmail.com>
2015-04-11 17:37:04 -0700

    Added Lab0 files: Initial Commit

---
+++ Lab1/bit_subtractor.v
@@ -0,0 +1,16 @@
+module bit_subtractor(bit_a, bit_b, bit_borrow_in, bit_diff, bit_borrow_out, start, done);
+
+input bit_a;
+input bit_b;
+input bit_borrow_in;
+input start;
+
+output bit_diff;
+output bit_borrow_out;
+output done;
+
+assign bit_diff = (bit_a ^ bit_b) ^ bit_borrow_in;
+assign bit_borrow_out = ((~bit_a & bit_borrow_in) | (~bit_a & bit_b) | (bit_b & bit_borrow_in));
+assign done = start;
+
+endmodule
