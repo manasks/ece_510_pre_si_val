99402e69501c92f2900f12edc9a981e1b27975bc
Manas Kanithahalli Sadananda <manas.ks15@gmail.com>
2015-05-09 12:18:50 -0700

    added Lab1 solutions chkr

--- Lab2/alu_chkr.v
+++ Lab2/alu_chkr.v
@@ -1,311 +1,197 @@
-`timescale 1ns/1ps
-`include "alu.pkg"
-module alu_chkr(clk, reset_n, opcode_valid, opcode, data, result, overflow, done, checker_enable);
+// =======================================================================
+//   Filename:     alu_chkr.v
+//   Created by:   Tareque Ahmad
+//   Date:         April 20, 2014
+//   ECE 510: Pre-Silicon Validation
+//   Lab 1
+//   This is the checker module meant to be used to check functionality
+//   of simple_alu module done in Lab0
+//
+//   The rules for checking are as follows:
+//   Check# 1: When reset_n is asserted (driven to 0),
+//   all outputs become 0 within 1 clock cycle.
+//
+//   Check# 2: When opcode_valid is asserted, valid opcode 
+//   and valid data (no X or Z) must be driven on the same cycle.
+//
+//   Check# 3: Output "done" must be asserted within 2 cycles 
+//   after both valid data have been captured
+//
+//   Check# 4: Once "done" is asserted, output "result" must 
+//   be correct on the same cycle.
+//
+//   Check# 5: Once "done" is asserted, output "overflow" must 
+//   be correct on the same cycle
+//
+// =======================================================================
 
-input clk;
-input reset_n;
-input opcode_valid;
-input opcode;
-input [DATA_WIDTH-1:0] data;
-input [DATA_WIDTH-1:0] result;
-input overflow;
-input done;
-input [4:0] checker_enable;
+// Let's define some defines
+`define DATA_WIDTH 8
 
-reg [DATA_WIDTH:0] result_buf;
+module alu_chkr
+  (
+   // Global inputs
+   input                     clk,
+   input                     reset_n,
 
-reg [DATA_WIDTH-1:0] data_A;
-reg [DATA_WIDTH-1:0] data_B;
+   // Control inputs
+   input                     opcode_valid,
+   input                     opcode,
 
-reg [1:0] opcode_buf;
+   // Data inputs
+   input [`DATA_WIDTH-1:0]   data,
 
-reg chkr1_State;
-reg [1:0] chkr3_State;
-reg [1:0] chkr4_State;
-reg [1:0] chkr5_State;
+   // 
+   input                    done,
+   input                    overflow,
+   input  [`DATA_WIDTH-1:0] result
 
-parameter chkr1_CHECK = 1'b1;
-parameter chkr1_RESET = 1'b0;
+   );
 
-parameter chkr3_OPCODE1 = 2'b00;
-parameter chkr3_OPCODE2 = 2'b01;
-parameter chkr3_COMPUTE = 2'b10;
-parameter chkr3_CHECK = 2'b11;
+   parameter OPCODE_ADD = 2'b00;
+   parameter OPCODE_SUB = 2'b01;
+   parameter OPCODE_PAR = 2'b10;
+   parameter OPCODE_COMP = 2'b11;
 
-parameter chkr4_OPCODE1 = 2'b00;
-parameter chkr4_OPCODE2 = 2'b01;
-parameter chkr4_DONE = 2'b10;
+   reg [2:0] opcode_valid_count;
+   reg [2:0] reset_count;
 
-parameter chkr5_OPCODE1 = 2'b00;
-parameter chkr5_OPCODE2 = 2'b01;
-parameter chkr5_DONE = 2'b10;
+   reg [`DATA_WIDTH-1:0] calc_result;
+   reg                   calc_overflow;
 
-initial
-begin
-    chkr1_State = chkr1_RESET;
-    chkr3_State = chkr3_OPCODE1;
-    chkr4_State = chkr4_OPCODE1;
-    chkr5_State = chkr5_OPCODE1;
-end
+   reg [`DATA_WIDTH-1:0] data_a;
+   reg [`DATA_WIDTH-1:0] data_b;
+   reg [1:0] int_opcode;
 
-always @(posedge clk)
-begin
-    //$display("opcode: %h \t opcode_valid: %h \t data: %h \t result: %h \t overflow: %h \t done: %h",opcode, opcode_valid, data, result, overflow, done);
-end
+   // Generate some counters
+   always @(posedge clk) begin
+      if (opcode_valid)
+         opcode_valid_count = opcode_valid_count+1;
+      else
+         opcode_valid_count = 0;
 
-//Checker 1: when reset_n is asserted (driven to 0), all outputs become 0
-//within 1 clock cycle.
-always @(posedge clk)
-begin
-    case(chkr1_State)
-		chkr1_RESET:
-		begin
-			if(reset_n && checker_enable[0])
-			begin
-				chkr1_State = chkr1_CHECK;
-			end
-			else
-			begin
-				chkr1_State = chkr1_RESET;
-			end
-		end
-		
-		chkr1_CHECK:
-		begin
-			if (!data && !overflow && !done)
-			begin
-				$display("CHECKER 1 FAILED");
-			end
-			else
-			begin
-				$display("CHECKER 1 PASSED");
-			end
-			chkr1_State = chkr1_RESET;
-		end
-	endcase
-end
+      if (reset_n == 1)
+         reset_count = 0;
+      else if (reset_count <= 4)
+         reset_count = reset_count+1;
+      else
+         reset_count = reset_count;
+   end
 
-//Checker 2: when opcode_valid is asserted, valid opcode and valid data
-//(no X or Z) must be driven on the same cycle.
-always @(posedge clk)
-begin
-    if (opcode_valid && checker_enable[1])
-    begin
-       if(opcode == "x" || data == "xxxxxxxx")
-       begin
-            $display("CHECKER 2 FAILED");
-       end
-       else if(opcode == "z" || data == "zzzzzzzz")
-       begin
-            $display("CHECKER 2 FAILED");
-       end
-       else
-       begin
-            $display("CHECKER 2 PASSED");
-       end
-    end
-end
+   // Capture both data operands on subsequent cycles
+   always @(posedge clk) begin
+      if (opcode_valid_count == 0) begin
+         data_a     = 0;
+         data_b     = 0;
+         int_opcode = 0;
+      end 
+      else if (opcode_valid_count == 1) begin
+         data_a = data;
+         int_opcode[0] = opcode;
+      end
 
-//Checker 3: Output “done” must be asserted within 2 cycles after both valid data 
-//have been captured.
-always @(posedge clk)
-begin
-    case(chkr3_State)
-        chkr3_OPCODE1:
-		begin
-			if (opcode_valid && checker_enable[2])
-			begin
-				chkr3_State = chkr3_OPCODE2;
-			end
-			else
-			begin
-				chkr3_State = chkr3_OPCODE1;
-			end
-		end
-		
-		chkr3_OPCODE2:
-		begin
-			if (opcode_valid)
-			begin
-				chkr3_State = chkr3_COMPUTE;
-			end
-			else
-			begin
-				chkr3_State = chkr3_OPCODE2;
-			end
-		end
-		
-		chkr3_COMPUTE:
-		begin
-			chkr3_State = chkr3_CHECK;
-		end
-		
-		chkr3_CHECK:
-		begin
-			if (done)
-			begin
-				$display("CHECKER 3 PASSED");
-				chkr3_State = chkr3_OPCODE1;
-			end
-			else
-			begin
-				$display("CHECKER 3 FAILED");
-                chkr3_State = chkr3_OPCODE1;
-			end
-		end
-	endcase	
-end
+      else if (opcode_valid_count == 3) begin
+         data_b = data;
+         int_opcode[1] = opcode;
+      end
 
-//Checker 4: Once “done’ is asserted, output “result” must be correct
-//on the same cycle.
-always @(posedge clk or done or opcode_valid or result or data or overflow or checker_enable[3])
-begin
-	case(chkr4_State)
-		chkr4_OPCODE1:
-		begin
-			if(opcode_valid && checker_enable[3])
-			begin
-				data_A = data;
-				opcode_buf[0] = opcode;
-				chkr4_State = chkr4_OPCODE2;
-			end
-			else
-			begin
-				chkr4_State = chkr4_OPCODE1;
-			end
-		end
-		
-		chkr4_OPCODE2:
-		begin
-			if(opcode_valid)
-			begin
-				data_B = data;
-				opcode_buf[1] = opcode;
-				chkr4_State = chkr4_DONE;
-			end
-			else
-			begin
-				chkr4_State = chkr4_OPCODE2;
-			end
-		end
-		
-		chkr4_DONE:
-		begin
-			if(done)
-			begin
-				case(opcode_buf)
-					2'b00:
-					begin
-						result_buf = data_A + data_B;
-					end
-					
-					2'b01:
-					begin
-						result_buf = data_A - data_B;
-					end
-					
-					2'b10:
-					begin
-						result_buf = data_A ^ data_B;
-					end
-					
-					2'b11:
-					begin
-						result_buf = data_A ~^ data_B;
-					end
-				endcase
-				if (result == result_buf[DATA_WIDTH-1:0])
-				begin
-					$display("CHECKER 4 PASSED");
-				end
-				else
-				begin
-					$display("CHECKER 4 FAILED");
-				end
-				chkr4_State = chkr4_OPCODE1;
-			end
-			else
-			begin
-				chkr4_State = chkr4_OPCODE1;
-			end
-		end
-	endcase
-end
+   end
 
-//Checker 5: Once “done’ is asserted, output “overflow” must be correct on the 
-//same cycle.
-always @(posedge clk or done or overflow or checker_enable[4])
-begin
-	case(chkr5_State)
-		chkr5_OPCODE1:
-		begin
-			if(opcode_valid && checker_enable[3])
-			begin
-				data_A = data;
-				opcode_buf[0] = opcode;
-				chkr5_State = chkr5_OPCODE2;
-			end
-			else
-			begin
-				chkr5_State = chkr5_OPCODE1;
-			end
-		end
-		
-		chkr5_OPCODE2:
-		begin
-			if(opcode_valid)
-			begin
-				data_B = data;
-				opcode_buf[1] = opcode;
-				chkr5_State = chkr5_DONE;
-			end
-			else
-			begin
-				chkr5_State = chkr5_OPCODE2;
-			end
-		end
-		
-		chkr5_DONE:
-		begin
-			if(done)
-			begin
-				case(opcode_buf)
-					2'b00:
-					begin
-						result_buf = data_A + data_B;
-					end
-					
-					2'b01:
-					begin
-						result_buf = data_A - data_B;
-					end
-					
-					2'b10:
-					begin
-						result_buf = data_A ^ data_B;
-					end
-					
-					2'b11:
-					begin
-						result_buf = data_A ~^ data_B;
-					end
-				endcase
-				if (overflow == result_buf[DATA_WIDTH])
-				begin
-					$display("CHECKER 5 PASSED");
-				end
-				else
-				begin
-					$display("CHECKER 5 FAILED");
-				end
-				chkr5_State = chkr5_OPCODE1;
-			end
-			else
-			begin
-				chkr5_State = chkr5_OPCODE1;
-			end
-		end
-	endcase
-end
+   `ifndef DISABLE_CHECK1
+      // Code for Check# 1
+      always @(posedge clk) begin
+         if (reset_count == 3) begin
+            if (done !== 0 | result !== 0 | overflow !== 0) begin
+               $display ("\n@ %0d ns: ALU CHKR: Error: Outputs are not zero after an assertion of reset.", $time);
+            end
+         end
+      end
+   `endif
 
-endmodule
+   `ifndef DISABLE_CHECK2
+      // Code for Check# 2
+      always @(posedge clk) begin
+         if (opcode_valid) begin
+            if (opcode !== 1'b0 && opcode !== 1'b1) begin
+               $display ("\n@ %0d ns: ALU CHKR: Error: Invalid opcode is detected while opcode_valid is asserted.", $time);
+            end
+
+            if ((data[0] !== 0 && data[0] !== 1) ||
+                (data[1] !== 0 && data[1] !== 1) ||
+                (data[2] !== 0 && data[2] !== 1) ||
+                (data[3] !== 0 && data[3] !== 1) ||
+                (data[4] !== 0 && data[4] !== 1) ||
+                (data[5] !== 0 && data[5] !== 1) ||
+                (data[6] !== 0 && data[6] !== 1) ||
+                (data[7] !== 0 && data[7] !== 1)) begin
+               $display ("\n@ %0d ns: ALU CHKR: Error: Invalid data is detected while opcode_valid is asserted.", $time);
+            end
+
+            calc_outputs();
+         end
+      end
+   `endif
+
+   `ifndef DISABLE_CHECK3
+      // Code for Check# 3
+      always @(posedge clk) begin
+         if (opcode_valid_count == 5) begin
+            if (done !== 1) begin
+               $display ("\n@ %0d ns: ALU CHKR: Error: Done is not set within two cycles of opcode_valid.", $time);
+            end
+         end
+      end
+   `endif
+
+   `ifndef DISABLE_CHECK4
+      // Code for Check# 4
+      always @(posedge clk) begin
+         if (opcode_valid_count == 5) begin
+            if (result !== calc_result) begin
+               $display ("\n@ %0d ns: ALU CHKR: Error: Result does not match with expected results.", $time);
+               $display ("\n@ %0d ns: ALU CHKR: Error: Expected: %h. Actual: %h for opcode= %h , data_a= %h, data_b= %h", $time, calc_result, result, int_opcode, data_a, data_b);
+            end
+         end
+      end
+   `endif
+
+   `ifndef DISABLE_CHECK5
+      // Code for Check# 4
+      always @(posedge clk) begin
+         if (opcode_valid_count == 5) begin
+            if (overflow !== calc_overflow) begin
+               $display ("\n@ %0d ns: ALU CHKR: Error: Overflow does not match with expected overflow.", $time);
+               $display ("\n@ %0d ns: ALU CHKR: Error: Expected: %h. Actual: %h for opcode= %h , data_a= %h, data_b= %h", $time, calc_overflow, overflow, int_opcode, data_a, data_b);
+            end
+         end
+      end
+   `endif
+
+   // Task to calculate expected results
+   task calc_outputs;
+   begin
+      case(int_opcode)
+         OPCODE_ADD:   begin
+                  {calc_overflow,calc_result} <= data_a + data_b;
+               end
+
+         OPCODE_SUB:   begin
+                  {calc_overflow,calc_result} <= data_a - data_b;
+               end
+
+         OPCODE_PAR:   begin
+                  calc_result   <= data_a ^ data_b;
+                  calc_overflow <= ^calc_result;
+               end
+
+         OPCODE_COMP:   begin
+                  calc_result   <= data_a ~^ data_b;
+                  calc_overflow <= 0;
+               end
+      endcase
+   end   
+   endtask
+
+
+endmodule //  alu_chkr
+
